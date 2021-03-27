cabc6bdd13925d0e6afc41c8fe7ef2a19af8ee94
Jesse Klein <jesse10klein@gmail.com>
2021-03-27 17:23:18 +1000

    Added more endpoints and pagination handler

--- SwyftxWrapper.js
+++ SwyftxWrapper.js
@@ -2,12 +2,20 @@ const axios = require("axios");
 
 const baseUrl = "https://api.swyftx.com.au";
 
-async function axiosRequest(method, url, data) {
-  const fullUrl = `${baseUrl}${url}`;
-  const response = await axios({method, fullUrl, data})
+async function axiosRequest(method, url, headers) {
+  const response = await axios({method, url: `${baseUrl}${url}`, headers})
                     .then(resp => resp.data)
-                    .catch(err => err.response.data.error);
+                    .catch(err => {console.log(err); err.response.data.error});
   return response;
+}
+
+function paginationHandler(options) {
+  let optionsPresent = [];
+  if (options.limit) optionsPresent.push(`limit=${options.limit}`);
+  if (options.page) optionsPresent.push(`page=${options.page}`)
+  if (options.sortBy) optionsPresent.push(`sortBy=${options.sortBy}`)
+  const optionsString = optionsPresent.join("&");
+  return "?" + optionsString;
 }
 
 function Swyftx(apiKey) {
@@ -17,9 +25,9 @@ function Swyftx(apiKey) {
 
   self.getAccessToken = () => self.accessToken;
 
-  self.setDefaultHeaders = (headers) => {
-    headers["Content-Type"] = "Application/json";
-    headers["Authorization"] = `Bearer ${self.accessToken}`;
+  self.getHeaders = (needsAuth) => {
+    headers = {"Content-Type": "application/json"};
+    if (needsAuth) headers["Authorization"] = `Bearer ${self.accessToken}`;
     return headers;
   }
 
@@ -29,34 +37,46 @@ function Swyftx(apiKey) {
     self.accessToken = accessToken;
     return accessToken;
   }
-  
+
+  //History endpoints
+
+  self.getCurrencyWithdrawHistory = async (options) => {
+    const url = `/history/withdraw/${paginationHandler(options)}`;
+    const response = await axiosRequest("GET", url, this.getHeaders(true));
+    return response;
+  }
+
+
+  //Limits endpoints
+
+  //WORKING
+  self.getWithdrawalLimits = async() => {
+    const response = await axiosRequest("GET", "/limits/withdrawal", this.getHeaders(true));
+    return response;
+  }
+
+  //Market endpoints
+
+  //NEED TO CHECK
+  self.getLiveRates = async (coin) => {
+    const response = await axiosRequest("GET", `/live-rates/${coin}/`, this.getHeaders(false));
+    return response;
+  }
+
+  //WORKING
+  self.getMarketAssets = async () => {
+    const response = await axiosRequest("GET", "/markets/assets", this.getHeaders(false));
+    return response;
+  }
+  //WORKING
   self.getBasicInfo = async (coin) => {
-    const response = await axiosRequest("GET", `markets/info/basic/${coin}/`, this.setDefaultHeaders({}));
+    const response = await axiosRequest("GET", `/markets/info/basic/${coin}/`, this.getHeaders(false));
     return response;
   }
-
-  self.logout = async () => {
-    const response = await axiosRequest("POST", "/auth/logout", this.setDefaultHeaders({}));
+  //WORKING
+  self.getDetailedInfo = async (coin) => {
+    const response = await axiosRequest("GET", `/markets/info/detail/${coin}`, this.getHeaders(false));
     return response;
-  }
-
-  self.getScope = async () => {
-    const response = await axiosRequest("GET", "/user/apiKeys/scope/", this.setDefaultHeaders({}));
-    return response;
-  }
-
-  self.getKeys = async () => {
-    const response = await axiosRequest("GET", "/user/apiKeys", this.setDefaultHeaders({}));
-    return response;
-  }
-
-  self.revokeKey = async (keyId) => {
-    const headers = this.setDefaultHeaders(keyId);
-    const reponse = await aciosRequest("POST", "/user/apiKeys/revoke", headers);
-  }
-
-  self.revokeAllKeys = async () => {
-    const reponse = await aciosRequest("POST", "/user/apiKeys/revokeAll", this.setDefaultHeaders({}));
   }
 
 }
