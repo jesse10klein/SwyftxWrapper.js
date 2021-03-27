badf861526cb836ac20cb576820bfde31e018486
Jesse Klein <jesse10klein@gmail.com>
2021-03-27 16:20:21 +1000

    Added some more endpoints (not tested)

--- SwyftxWrapper.js
+++ SwyftxWrapper.js
@@ -1,7 +1,10 @@
 const axios = require("axios");
 
+const baseUrl = "https://api.swyftx.com.au";
+
 async function axiosRequest(method, url, data) {
-  const response = await axios({method, url, data})
+  const fullUrl = `${baseUrl}${url}`;
+  const response = await axios({method, fullUrl, data})
                     .then(resp => resp.data)
                     .catch(err => err.response.data.error);
   return response;
@@ -14,21 +17,47 @@ function Swyftx(apiKey) {
 
   self.getAccessToken = () => self.accessToken;
 
+  self.setDefaultHeaders = (headers) => {
+    headers["Content-Type"] = "Application/json";
+    headers["Authorization"] = `Bearer ${self.accessToken}`;
+    return headers;
+  }
+
   self.generateRefreshToken = async () => {
-    const response = await axiosRequest("POST", "https://api.swyftx.com.au/auth/refresh/", {apiKey: self.key});
-    console.log(response);
+    const response = await axiosRequest("POST", "/auth/refresh/", {apiKey: self.key});
     const accessToken = response.accessToken;
     self.accessToken = accessToken;
     return accessToken;
   }
   
   self.getBasicInfo = async (coin) => {
-    const response = await axiosRequest("GET", `https://api.swyftx.com.au/markets/info/basic/${coin}/`, {
-      Authorization: `:Bearer ${self.accessToken}`
-    })
+    const response = await axiosRequest("GET", `markets/info/basic/${coin}/`, this.setDefaultHeaders({}));
     return response;
   }
 
+  self.logout = async () => {
+    const response = await axiosRequest("POST", "/auth/logout", this.setDefaultHeaders({}));
+    return response;
+  }
+
+  self.getScope = async () => {
+    const response = await axiosRequest("GET", "/user/apiKeys/scope/", this.setDefaultHeaders({}));
+    return response;
+  }
+
+  self.getKeys = async () => {
+    const response = await axiosRequest("GET", "/user/apiKeys", this.setDefaultHeaders({}));
+    return response;
+  }
+
+  self.revokeKey = async (keyId) => {
+    const headers = this.setDefaultHeaders(keyId);
+    const reponse = await aciosRequest("POST", "/user/apiKeys/revoke", headers);
+  }
+
+  self.revokeAllKeys = async () => {
+    const reponse = await aciosRequest("POST", "/user/apiKeys/revokeAll", this.setDefaultHeaders({}));
+  }
 
 }
 
